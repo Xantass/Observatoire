00bc315d14e302b45a4abe6386b9c1be067ff1dd
Theo Fouque <theo.fouque@epitech.eu>
2022-11-04 16:34:49 +0100

    [ADD] day 04/11/2022

--- js/script_operation.js
+++ js/script_operation.js
@@ -1,17 +1,23 @@
 var past = 0;
 var lot = 0;
+var slide_nb = 0;
 let lot_e;
 let projet_e;
 let image_e;
+let button_e;
 
 function load_data() {
     console.log("DATA");
     lot_e = document.getElementsByClassName("container of the lot");
     projet_e = document.getElementById("main information of the list of the operation");
     image_e = document.getElementById("1");
+    button_e = document.getElementsByClassName("button info");
 
     for (i = 0; i < lot_e.length; i++) {
         lot_e[i].addEventListener('click', change_background);
+    }
+    for (i = 0; i < button_e.length; i++) {
+        button_e[i].addEventListener('click', select_form_operation);
     }
     projet_e.addEventListener('click', change_background);
     image_e.addEventListener('click', get_lot);
@@ -88,8 +94,59 @@ function change_background (e) {
 
     e.stopPropagation();
     projet_background.style.background = "none";
+    projet_background.style.fontWeight = "normal";
     for (i = 0; i < lot_background.length; i++) {
         lot_background[i].style.background = "none";
+        lot_background[i].style.fontWeight = "normal";
     }
     this.style.backgroundColor = "#45bab85d";
+    this.style.fontWeight = "bold";
+}
+
+function slide() {
+    var image = document.getElementById("arrow");
+    var container = document.getElementById("container nav operation");
+    var box = document.getElementById("list element of the operation");
+    var button = document.getElementById("extented the nav");
+    var form = document.getElementById("container form");
+
+    console.log(this);
+    console.log(container.style.Maxheight);
+    if (slide_nb == 0) {
+        box.style.display = "none";
+        image.src = "/image/right_arrow.png";
+        container.style.transform = "translateX(-320px)";
+        button.style.marginTop = container.style.maxHeight;
+        form.style.marginLeft = "-320px";
+        slide_nb = 1;
+    }
+    else {
+        image.src = "/image/left_arrow.png";
+        container.style.transform = "translateX(0px)";
+        box.style.display = "flex";
+        button.style.marginTop = "0px"
+        form.style.marginLeft = "0px";
+        slide_nb = 0;
+    }
+}
+
+function select_form_operation(e) {
+    var generale = document.getElementById("container form generale");
+    var dimension = document.getElementById("container form dimensionnelle");
+
+    for (i = 0; i < button_e.length; i++) {
+        button_e[i].style.backgroundColor = "#fff";
+        button_e[i].style.color = "#22334d";
+    }
+    e.stopPropagation();
+    this.style.color = "#fff";
+    this.style.backgroundColor = "#22334d";
+    if(this.id == "button info generale") {
+        generale.style.display = "block";
+        dimension.style.display = "none";
+    }
+    else {
+        generale.style.display = "none";
+        dimension.style.display = "block";
+    }
 }
